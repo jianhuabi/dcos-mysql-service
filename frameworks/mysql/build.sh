721329feec08895a602b46b8ab7f8e818c221bc1
root <root@ip-172-16-24-16.ec2.internal>
2019-09-05 16:36:34 +0000

    clean build folder

--- frameworks/mysql/build.sh
+++ frameworks/mysql/build.sh
@@ -10,15 +10,13 @@ source $FRAMEWORK_DIR/versions.sh
 # Build/test scheduler.zip/CLIs/setup-helper.zip
 ${REPO_ROOT_DIR}/gradlew -p ${FRAMEWORK_DIR} check distZip
 $FRAMEWORK_DIR/cli/build.sh
-$FRAMEWORK_DIR/setup-helper/build.sh
 
 # Build package with our scheduler.zip/CLIs/setup-helper.zip and the SDK artifacts we built:
 $REPO_ROOT_DIR/tools/build_package.sh \
-    kafka \
+    mysql \
     $FRAMEWORK_DIR \
     -a "$FRAMEWORK_DIR/build/distributions/$(basename $FRAMEWORK_DIR)-scheduler.zip" \
     -a "$FRAMEWORK_DIR/cli/dcos-service-cli-linux" \
     -a "$FRAMEWORK_DIR/cli/dcos-service-cli-darwin" \
     -a "$FRAMEWORK_DIR/cli/dcos-service-cli.exe" \
-    -a "$FRAMEWORK_DIR/setup-helper/setup-helper.zip" \
     $@
