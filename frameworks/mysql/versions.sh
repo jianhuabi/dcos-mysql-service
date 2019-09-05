419715f2b673d8a48cd6f291eeac7dc87764a96b
root <root@ip-172-16-24-16.ec2.internal>
2019-09-05 17:03:04 +0000

    Initial pass version: 0.1

--- frameworks/mysql/versions.sh
+++ frameworks/mysql/versions.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# This `TEMPLATE_KAFKA_VERSION` string is composed of:
-
-
 # To upload a new version of the Mysql package to that repository, follow the
 # instructions at:
 # https://wiki.mesosphere.com/display/ENGINEERING/Uploading+an+asset+to+production
-export TEMPLATE_KAFKA_VERSION="5.7.25"
+export TEMPLATE_MYSQL_VERSION="5.7.25"
 export TEMPLATE_DCOS_SDK_VERSION="0.56.2"
